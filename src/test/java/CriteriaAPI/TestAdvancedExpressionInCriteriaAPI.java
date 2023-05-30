fa8fbc6ece0e4f6af8d731613575199ef3e0029a
aelkassimi <aelkassimi@sqli.com>
2023-05-30 15:55:30 +0100

    Advanced Expression in Criteria API (Predicate)

---
+++ src/test/java/CriteriaAPI/TestAdvancedExpressionInCriteriaAPI.java
@@ -0,0 +1,63 @@
+package CriteriaAPI;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.example.relationships.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestAdvancedExpressionInCriteriaAPI {
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
+
+    @Before
+    public void setUp() {
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        sessionFactory = configuration.buildSessionFactory();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+    }
+
+    @After
+    public void cleanUp() {
+        transaction.rollback();
+        session.close();
+        sessionFactory.close();
+    }
+
+    @Test
+    public void testCriteriaAPIAdvancedExpressions() {
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Book> query = criteriaBuilder.createQuery(Book.class);
+        Root<Book> bookRoot = query.from(Book.class);
+
+        // predicates
+        Predicate likePredicate = criteriaBuilder.like(bookRoot.get("title"), "Atomic Habits");
+        Predicate betweenPredicate = criteriaBuilder.between(bookRoot.get("price"), 0, 8);
+        Predicate isNullPredicate = criteriaBuilder.isNull(bookRoot.get("isbn"));
+        Predicate inPredicate = bookRoot.get("publishDate").in(LocalDateTime.of(2023,6,30,15,1,30),LocalDateTime.of(2023,6,30,15,1,13));
+
+
+        Predicate combinePredicate = criteriaBuilder.and(likePredicate, betweenPredicate, isNullPredicate ,inPredicate);
+        // execute the query
+        query.where(combinePredicate);
+
+        List<Book> books = session.createQuery(query).getResultList();
+        assertNotNull(books);
+        assertEquals(1,books.size());
+    }
+}
